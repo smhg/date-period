be8b8e3d4e5538883970bd789657b6ef8441cef8
smhg <sam@strictlyphp.com>
2013-12-01 17:09:01 +0100

    toString method

--- test/period_test.js
+++ test/period_test.js
@@ -10,14 +10,23 @@ module.exports = {
     test.throws(function () { new Period(); }, 'constructor should expect parameters');
     test.throws(function () { new Period('2013-12-01'); }, 'constructor should not allow invalid parameters');
 
-    period = new Period(start, moment.duration(1, 'day'), 6);
+    period = new Period(start, moment.duration(1, 'day'), 7);
 
     test.deepEqual(period[0], start, 'first item should match start');
-    test.equal(period.length, 7, 'length property should match number of occurences');
+    test.equal(period.length, 8, 'length property should match number of occurences (recurrences + 1!)');
 
     period = new Period(start, moment.duration(1, 'day'), end);
 
     test.deepEqual(period[period.length - 1], end, 'last item should match end');
+
+    test.done();
+  },
+  'toString': function toString (test) {
+    var start = moment(),
+      end = start.clone().add(7, 'days'),
+      period = new Period(start, moment.duration(1, 'day'), end);
+
+    test.ok(period.toString().length > 3, 'conversion to string should work');
 
     test.done();
   }
